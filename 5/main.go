71e650b10d9be777412a2140df4cf6aa33e1f30f
solipsis <davida3141@gmail.com>
2019-12-09 11:41:59 -0700

    part 2

--- 5/main.go
+++ 5/main.go
@@ -35,15 +35,16 @@ func solveA(r io.Reader, input int) int {
 		}
 	}
 
+	result := 0
 	pc := 0
 	for {
 		op := state[pc] % 100
 		m1 := (state[pc] / 100) % 10
 		m2 := (state[pc] / 1000) % 10
-		m3 := (state[pc] / 10000) % 10
+		//m3 := (state[pc] / 10000) % 10
 
-		fmt.Printf("op: %d, m1: %d, m2: %d, m3: %d, pc: %d\n", op, m1, m2, m3, pc)
-		fmt.Printf("%v\n", state[pc:pc+4])
+		//fmt.Printf("op: %d, m1: %d, m2: %d, m3: %d, pc: %d\n", op, m1, m2, m3, pc)
+		//fmt.Printf("%v\n", state[pc:pc+4])
 		switch op {
 		case 1:
 			state[state[pc+3]] = fetch(pc+1, m1) + fetch(pc+2, m2)
@@ -59,6 +60,7 @@ func solveA(r io.Reader, input int) int {
 			pc += 2
 		case 4:
 			fmt.Println("OUT:", fetch(pc+1, m1))
+			result = fetch(pc+1, m1)
 			pc += 2
 		case 5: // jump-if-true
 			if fetch(pc+1, m1) != 0 {
@@ -88,7 +90,7 @@ func solveA(r io.Reader, input int) int {
 			pc += 4
 
 		case 99:
-			return state[0]
+			return result
 		default:
 			panic("invalid opcode: " + strconv.Itoa(op))
 		}
@@ -106,55 +108,6 @@ func fetch(i int, mode int) int {
 		panic("invalid paramater mode: " + strconv.Itoa(mode))
 	}
 }
-
-/*
-func solveB(r io.Reader) int {
-	sc := bufio.NewScanner(r)
-	initial := make([]int, 0)
-
-	for sc.Scan() {
-		nums := strings.Split(sc.Text(), ",")
-		for _, v := range nums {
-			i, err := strconv.Atoi(v)
-			if err != nil {
-				panic(err)
-			}
-			initial = append(initial, i)
-		}
-	}
-
-	for noun := 1; noun < len(initial); noun++ {
-		for verb := 1; verb < len(initial); verb++ {
-			state := make([]int, len(initial))
-			copy(state, initial)
-			if execProgram(state, noun, verb) == 19690720 {
-				return 100*noun + verb
-			}
-		}
-	}
-	return -1
-}
-
-func execProgram(state []int, noun, verb int) int {
-	state[1] = noun
-	state[2] = verb
-	for pc := 0; ; pc += 4 {
-		switch state[pc] {
-		case 1:
-			state[state[pc+3]] = state[state[pc+1]] + state[state[pc+2]]
-			break
-		case 2:
-			state[state[pc+3]] = state[state[pc+1]] * state[state[pc+2]]
-			break
-		case 99:
-			return state[0]
-		default:
-			panic("invalid opcode")
-		}
-	}
-}
-
-*/
 func open(fname string) io.Reader {
 	f, err := os.Open("input.txt")
 	if err != nil {
@@ -165,7 +118,7 @@ func open(fname string) io.Reader {
 
 func main() {
 	input := open("input.txt")
-	fmt.Printf("A: %d\n", solveA(input, 5))
-	//input = open("input.txt")
-	//fmt.Printf("B: %d\n", solveB(input))
+	fmt.Printf("A: %d\n", solveA(input, 1))
+	input = open("input.txt")
+	fmt.Printf("B: %d\n", solveA(input, 5))
 }
