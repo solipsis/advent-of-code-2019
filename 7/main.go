a01432e69625c5924ed413810edd749019eb9b00
solipsis <davida3141@gmail.com>
2019-12-09 19:12:46 -0700

    very ugly challeng 7b

--- 7/main.go
+++ 7/main.go
@@ -18,75 +18,76 @@ const (
 
 var mode parameterMode = position
 
-var state []int
+//var state []int
 
-func execProgram(program, input []int) int {
-	fmt.Printf("Program: %v\n", program)
-	fmt.Printf("Input: %v\n", input)
+func (a *amplifier) init(program, input []int) {
+	a.state = make([]int, len(program))
+	copy(a.state, program)
+	a.input = input
+}
 
-	state = make([]int, len(program))
-	copy(state, program)
+func (a *amplifier) run() (int, bool) {
+	//fmt.Printf("input: %v\n", a.input)
 
-	result := 0
-	pc := 0
 	for {
-		op := state[pc] % 100
-		m1 := (state[pc] / 100) % 10
-		m2 := (state[pc] / 1000) % 10
-		//m3 := (state[pc] / 10000) % 10
+		op := a.state[a.pc] % 100
+		m1 := (a.state[a.pc] / 100) % 10
+		m2 := (a.state[a.pc] / 1000) % 10
+		//m3 := (a.state[a.pc] / 10000) % 10
 
-		//fmt.Printf("op: %d, m1: %d, m2: %d, m3: %d, pc: %d\n", op, m1, m2, m3, pc)
-		//fmt.Printf("%v\n", state[pc:pc+4])
+		//fmt.Printf("op: %d, m1: %d, m2: %d, m3: %d, a.pc: %d\n", op, m1, m2, m3, pc)
+		//fmt.Printf("%v\n", a.state[a.pc:pc+4])
 		switch op {
 		case 1:
-			state[state[pc+3]] = fetch(pc+1, m1) + fetch(pc+2, m2)
-			pc += 4
+			a.state[a.state[a.pc+3]] = a.fetch(a.pc+1, m1) + a.fetch(a.pc+2, m2)
+			a.pc += 4
 			break
 		case 2:
-			state[state[pc+3]] = fetch(pc+1, m1) * fetch(pc+2, m2)
-			pc += 4
+			a.state[a.state[a.pc+3]] = a.fetch(a.pc+1, m1) * a.fetch(a.pc+2, m2)
+			a.pc += 4
 			break
 		case 3:
-			if len(input) == 0 {
+			if len(a.input) == 0 {
 				panic("No more input available")
 			}
-			state[state[pc+1]] = input[0]
-			input = input[1:]
-			pc += 2
+			a.state[a.state[a.pc+1]] = a.input[0]
+			a.input = a.input[1:]
+			a.pc += 2
 		case 4:
-			fmt.Println("OUT:", fetch(pc+1, m1))
-			result = fetch(pc+1, m1)
-			pc += 2
+			out := a.fetch(a.pc+1, m1)
+			fmt.Println("OUT:", out)
+			a.pc += 2
+			return out, false
 		case 5: // jump-if-true
-			if fetch(pc+1, m1) != 0 {
-				pc = fetch(pc+2, m2)
+			if a.fetch(a.pc+1, m1) != 0 {
+				a.pc = a.fetch(a.pc+2, m2)
 			} else {
-				pc += 3
+				a.pc += 3
 			}
 		case 6: // jump-if-false
-			if fetch(pc+1, m1) == 0 {
-				pc = fetch(pc+2, m2)
+			if a.fetch(a.pc+1, m1) == 0 {
+				a.pc = a.fetch(a.pc+2, m2)
 			} else {
-				pc += 3
+				a.pc += 3
 			}
 		case 7: // less than
-			if fetch(pc+1, m1) < fetch(pc+2, m2) {
-				state[state[pc+3]] = 1
+			if a.fetch(a.pc+1, m1) < a.fetch(a.pc+2, m2) {
+				a.state[a.state[a.pc+3]] = 1
 			} else {
-				state[state[pc+3]] = 0
+				a.state[a.state[a.pc+3]] = 0
 			}
-			pc += 4
+			a.pc += 4
 		case 8: // equals
-			if fetch(pc+1, m1) == fetch(pc+2, m2) {
-				state[state[pc+3]] = 1
+			if a.fetch(a.pc+1, m1) == a.fetch(a.pc+2, m2) {
+				a.state[a.state[a.pc+3]] = 1
 			} else {
-				state[state[pc+3]] = 0
+				a.state[a.state[a.pc+3]] = 0
 			}
-			pc += 4
+			a.pc += 4
 
 		case 99:
 			fmt.Println("Done")
-			return result
+			return -1, true
 		default:
 			panic("invalid opcode: " + strconv.Itoa(op))
 		}
@@ -116,23 +117,58 @@ func solveA(r io.Reader) int {
 		copy(cpy, perm)
 		perms = append(perms, cpy)
 	}
-	perm([]int{0, 1, 2, 3, 4}, 0, visit)
+	perm([]int{5, 6, 7, 8, 9}, 0, visit)
 
 	max := 0
-	out := 0
 	for _, p := range perms {
-		fmt.Printf("perm: %v\n", p)
-		// first execution
-		out = execProgram(program, []int{p[0], 0})
-		for _, v := range p[1:] {
-			out = execProgram(program, []int{v, out})
+
+		// re-init for each permutation set
+		amplifiers := make([]*amplifier, 5)
+		for x := 0; x < len(amplifiers); x++ {
+			amp := &amplifier{}
+			amp.init(program, []int{p[x]})
+			amplifiers[x] = amp
 		}
-		if out > max {
-			max = out
+		amplifiers[0].input = append(amplifiers[0].input, 0)
+
+		last := 0
+		for i := 0; i < len(p); {
+			out, done := amplifiers[i].run()
+
+			// save prevous output of final amplifier
+			if i == len(p)-1 {
+				// check if this was the final cycle
+				if done {
+					if last > max {
+						max = last
+					}
+					break
+				}
+				fmt.Println("LAST: ", out)
+				last = out
+			}
+
+			if done && i == len(p)-1 {
+				fmt.Println("done out", out)
+				if out > max {
+					max = out
+					fmt.Printf("New Best: %d, perm: %v\n", max, p)
+				}
+				break
+			}
+			next := (i + 1) % len(amplifiers)
+			amplifiers[next].input = append(amplifiers[next].input, out)
+			i = next
 		}
 	}
 
 	return max
+}
+
+type amplifier struct {
+	state []int
+	input []int
+	pc    int
 }
 
 func perm(arr []int, i int, visit func(perm []int)) {
@@ -147,13 +183,13 @@ func perm(arr []int, i int, visit func(perm []int)) {
 	}
 }
 
-func fetch(i int, mode int) int {
+func (a *amplifier) fetch(i int, mode int) int {
 
 	switch parameterMode(mode) {
 	case position:
-		return state[state[i]]
+		return a.state[a.state[i]]
 	case immediate:
-		return state[i]
+		return a.state[i]
 	default:
 		panic("invalid paramater mode: " + strconv.Itoa(mode))
 	}
