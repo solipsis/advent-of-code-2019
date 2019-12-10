56908f871b1eb097af6a3ea6c003934603b5e122
solipsis <davida3141@gmail.com>
2019-12-09 18:07:24 -0700

    challeng 6 part 2

--- 6/main.go
+++ 6/main.go
@@ -9,15 +9,18 @@ import (
 )
 
 type node struct {
-	id     string
-	orbits map[string]*node
+	id      string
+	orbits  map[string]*node
+	links   map[string]*node
+	visited bool
 }
 
-var galaxy = make(map[string]*node)
+var galaxy map[string]*node
 
 func solveA(r io.Reader) int {
 	sc := bufio.NewScanner(r)
 
+	galaxy = make(map[string]*node)
 	galaxy["COM"] = &node{id: "COM", orbits: nil}
 	for sc.Scan() {
 		arr := strings.Split(sc.Text(), ")")
@@ -53,11 +56,71 @@ func dfs(n *node) int {
 	return sum
 }
 
+func solveB(r io.Reader) int {
+	sc := bufio.NewScanner(r)
+
+	galaxy = make(map[string]*node)
+	galaxy["COM"] = &node{id: "COM", links: make(map[string]*node)}
+	for sc.Scan() {
+		arr := strings.Split(sc.Text(), ")")
+		a, b := arr[0], arr[1]
+
+		if galaxy[a] == nil {
+			galaxy[a] = &node{id: a, links: make(map[string]*node)}
+		}
+		if galaxy[b] == nil {
+			galaxy[b] = &node{id: b, links: make(map[string]*node)}
+		}
+
+		// link in both directions
+		galaxy[b].links[a] = galaxy[a]
+		galaxy[a].links[b] = galaxy[b]
+	}
+
+	return bfs(galaxy["YOU"], "SAN") - 2
+}
+
+func bfs(n *node, target string) int {
+
+	queue := []*node{n, nil}
+	depth := 0
+	for len(queue) > 1 {
+		// pop top item
+		next := queue[0]
+		queue = queue[1:]
+
+		// check if depth has increased
+		if next == nil {
+			depth++
+			// depth marker
+			queue = append(queue, nil)
+			continue
+		}
+
+		fmt.Printf("visiting: %v, depth: %d\n", (*next).id, depth)
+
+		if next.id == target {
+			return depth
+		}
+
+		next.visited = true // prevent loops
+
+		// visit children
+		for _, l := range next.links {
+			if !l.visited {
+				queue = append(queue, l)
+			}
+		}
+	}
+
+	return -1
+}
+
 func main() {
 	input := open("input.txt")
 	fmt.Printf("A: %d\n", solveA(input))
-	//input = open("input.txt")
-	//fmt.Printf("B: %d\n", solveB(input))
+	input = open("input.txt")
+	fmt.Printf("B: %d\n", solveB(input))
 }
 
 func open(fname string) io.Reader {
--- 6/main_test.go
+++ 6/main_test.go
@@ -25,16 +25,26 @@ K)L`
 	}
 }
 
-/*
 func TestExampleB(t *testing.T) {
-	expected := 50346
-	input := "100756"
+	input := `COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN`
+	expected := 4
 	res := solveB(bytes.NewBufferString(input))
 	if res != expected {
 		t.Errorf("Got %d, expected %d", res, expected)
 	}
 }
-*/
 
 func TestA(t *testing.T) {
 	t.Logf("A: %d", solveA(open("input.txt")))
