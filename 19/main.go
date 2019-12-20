eacc865d0a20ff5a34e9e76d30cbc23d7de2261a
solipsis <davida3141@gmail.com>
2019-12-19 21:55:35 -0700

    challenge 19 part 2

--- 19/main.go
+++ 19/main.go
@@ -12,7 +12,107 @@ import (
 var grid [][]int
 
 func solveB(r io.Reader) int {
-	return -1
+	sc := bufio.NewScanner(r)
+
+	program := make([]int, 0)
+
+	for sc.Scan() {
+		nums := strings.Split(sc.Text(), ",")
+		for _, v := range nums {
+			i, err := strconv.Atoi(v)
+			if err != nil {
+				panic(err)
+			}
+			program = append(program, i)
+		}
+	}
+
+	sum := 0
+
+	// if row col == 1
+	// check +99 in all directions
+	// increase row by 1
+	// increase col until match
+	// repeat
+
+	c := newCPU(program)
+	//coords := []int{}
+	// construct input
+
+	size := 99
+
+	row := 15
+	col := 10
+	colStart := col
+	prevRes := 1
+
+	next := []int{col, row}
+	c.input = append(c.input, next...)
+
+	//62817 too low
+	for {
+
+		out, err := c.run()
+		switch err {
+		case nil:
+			c = newCPU(program)
+			c.input = append(c.input, next...)
+			continue
+		case errSuspend:
+			fmt.Printf("r: %d, c: %d, v: %d\n", row, col, out)
+			if out == 1 {
+
+				if prevRes == 0 {
+					colStart = col
+				}
+
+				// right
+				c = newCPU(program)
+				c.input = append(c.input, []int{col + size, row}...)
+				r, _ := c.run()
+				// down
+				c = newCPU(program)
+				c.input = append(c.input, []int{col, row + size}...)
+				d, _ := c.run()
+				// right down
+				c = newCPU(program)
+				c.input = append(c.input, []int{col + size, row + size}...)
+				rd, _ := c.run()
+
+				if r == 1 && d == 1 && rd == 1 {
+					return (col * 10000) + row
+				}
+
+				// check next column
+				col++
+				next = []int{col, row}
+			} else {
+				// if 2 consecutive misses go to new row
+				if prevRes == 0 {
+					row++
+					col = colStart
+					prevRes = 1
+					next = []int{col, row}
+					continue
+				} else {
+					// try next column
+					col++
+					next = []int{col, row}
+				}
+			}
+			prevRes = out
+
+		case errNeedInput:
+			//c.input = append(c.input, coords[0])
+			//prev = append(prev, coords[0])
+			//coords = coords[1:]
+			panic("shouldn't need more input?")
+
+		default:
+			panic(err)
+		}
+	}
+
 }
 
 func solveA(r io.Reader) int {
@@ -78,7 +178,7 @@ func open(fname string) io.Reader {
 
 func main() {
 	input := open("input.txt")
-	fmt.Printf("A: %d\n", solveA(input))
+	//fmt.Printf("A: %d\n", solveA(input))
 	//input = open("input.txt")
-	//fmt.Printf("B: %d\n", solveB(input))
+	fmt.Printf("B: %d\n", solveB(input))
 }
